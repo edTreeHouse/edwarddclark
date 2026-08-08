768493a4200444f72c43c31a8293948647d2b9a2
edTreeHouse <29818303+edTreeHouse@users.noreply.github.com>
2026-08-08 13:24:19 -0400

    Wire Site Observatory production runtime

---
+++ website/observatory/config.js
@@ -0,0 +1,7 @@
+window.EDC_OBSERVATORY_CONFIG = Object.freeze({
+  apiBaseUrl: 'https://3j1rrfpr45.execute-api.us-east-1.amazonaws.com',
+  cognitoDomain: 'https://auth.collectivestateinference.org',
+  clientId: '4ra2kpcmivarphppvbp98p25n5',
+  redirectUri: 'https://edwarddclark.com/observatory/',
+  logoutUri: 'https://edwarddclark.com/observatory/'
+});
